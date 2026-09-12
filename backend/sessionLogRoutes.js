e8fb3e7772c2647c64dda138642d3d733e8d30cc
Jana Nasser <nasserjana671@gmail.com>
2026-09-12 18:19:20 +0300

    Move sessionLogRoutes.js to backend/routes

--- backend/sessionLogRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createSessionLog, getSessionLogs } = require("../controllers/SessionLogController");
-const { protect, restrictTo } = require("../middlewares/authMiddleware");
-router.post("/logs", protect, createSessionLog);
-router.get("/logs/patient/:patientId", protect, restrictTo("patient", "therapist"), getSessionLogs);
-module.exports = router;

7c8e90e11e967ccea968c7a7751ab19d54c3e96b
rahma <rahmambrok0@gmail.com>
2026-09-12 16:01:03 +0300

    exercise update and delete

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,8 +1,12 @@
 const { createExercisePlan , getExercisePlans} = require("../controllers/ExercisePlanController");
+const { UpdateExercisePlan, DeleteExercisePlan , GetPatients } = require("../controllers/ExercisePlanUpdate&Delete");
 const express = require("express");
 const router = express.Router();
 
 router.post("/plans/:patientId", createExercisePlan);
 router.get("/plans/:patientId", getExercisePlans);
+router.put("/plans/:id",UpdateExercisePlan);
+router.delete("/plans/:id",DeleteExercisePlan),
+router.get("/users/patients", GetPatients);
 
 module.exports = router;

b4cfa8dee3fbfc1c64601a8a728da9adf4dd1b7f
Jana Nasser <nasserjana671@gmail.com>
2026-09-12 16:10:44 +0300

    Update Routes2.js

--- backend/routes/Routes2.js
+++ backend/routes/Routes2.js
@@ -1,12 +1,9 @@
-const { createExercisePlan, getExercisePlans } = require("../controllers/ExercisePlanController");
-const { createSessionLog, getSessionLogs } = require("../controllers/SessionLogController");
 const express = require("express");
 const router = express.Router();
+const { createSessionLog, getSessionLogs } = require("../controllers/SessionLogController");
+const { protect, restrictTo } = require("../middlewares/authMiddleware");
 
-router.post("/plans/:patientId", createExercisePlan);
-router.get("/plans/:patientId", getExercisePlans);
-
-router.post("/logs", createSessionLog);
-router.get("/logs/patient/:patientId", getSessionLogs);
+router.post("/logs", protect, createSessionLog);
+router.get("/logs/patient/:patientId", protect, restrictTo("patient", "therapist"), getSessionLogs);
 
 module.exports = router;

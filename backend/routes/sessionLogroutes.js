1bbeaac7d406bb2600ffa06af5e299968836ab5a
Jana Nasser <nasserjana671@gmail.com>
2026-09-13 03:07:53 +0300

    Refactor authentication middleware in sessionLogroutes

--- backend/routes/sessionLogroutes.js
+++ backend/routes/sessionLogroutes.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { createSessionLog, getSessionLogs } = require("../controllers/SessionLogController");
-const { protect } = require("../middlewares/authentication");
-const restrictTo = require("../middlewares/restrictTo");
+const { createSessionLog, getSessionLogs } = require("./controllers/SessionLogController");
+const { protect } = require("./middlewares/authentication");
+const restrictTo = require("./middlewares/restrictTo");
 
-router.post("/logs", protect, restrictTo("patient"), createSessionLog);
+const authenticate = (req, res, next) => {
+	Promise.resolve(protect(req, next)).catch(next);
+};
+
+router.post("/logs", authenticate, restrictTo("patient"), createSessionLog);
 router.get(
 	"/logs/patient/:patientId",
-	protect,
+	authenticate,
 	restrictTo("patient", "therapist"),
 	getSessionLogs
 );

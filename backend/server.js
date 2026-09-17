96639136e2d23802f949bd386438a87a4e557ebe
Mariam Khaled <mariam22khalid22@gmail.com>
2026-09-17 03:31:52 +0300

    fix: remove explicit env path from server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, ".env") });
-require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+require("dotenv").config();
 
 const app = require("./app");
 const chalk = require("chalk");
